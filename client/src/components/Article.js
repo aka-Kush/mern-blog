fcac51dab4e007542fb6b5bbb05a77c0b0b5ae8c
Shivansh Kush <kushivansh27@gmail.com>
2023-12-18 03:54:07 +0530

    integrated backend and frontend

--- client/src/components/Article.js
+++ client/src/components/Article.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import ArticleContent from './ArticleContent';
 import Articles from './Articles'
 import PageNotFound from './PageNotFound';
+import CommentsList from './CommentsList';
+import AddCommentForm from './AddCommentForm';
 
 const Article = () => {
     const { name } = useParams();
     const article = ArticleContent.find((article) => article.name === name);
+    const [articleInfo, setArticleInfo] = useState({ comments: [] });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json()
+            // console.log(body);
+            setArticleInfo(body);
+        };
+        fetchData();
+    }, [name]);
+
     if (!article) return <PageNotFound />;
     const otherArticles = ArticleContent.filter(article => article.name !== name);
     return (
@@ -19,6 +33,10 @@ const Article = () => {
                     <p className='mx-auto leading-relaxed text-base mb-4' key={index}>{para}</p>
                 ))
             }
+
+            <CommentsList comments={articleInfo.comments} />
+            <AddCommentForm articleName={name} setArticleInfo={setArticleInfo}/>
+            
             <h1 className='sm:text-2xl text-xl font-bold  mt-20 my-4 text-gray-900'>Other Articles</h1>
             <div className="flex flex-wrap -m-4">
                 <Articles articles={otherArticles}/>
