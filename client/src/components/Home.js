a5b945c027d20b68b2d88b4a9ae1fb9212db9bac
Shivansh Kush <kushivansh27@gmail.com>
2023-12-17 16:34:19 +0530

    updated home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Home = () => {
     return (
-      <div>Home</div>
+      <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6'>
+          Hello, Welcome to my Blog!
+        </h1>
+        <p className="mx-auto leading-relaxed text-base mb-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quos vero placeat doloribus suscipit provident odit reprehenderit excepturi odio, distinctio commodi dolorum, quia laborum quo molestiae perspiciatis porro tempore architecto facere, quisquam eveniet nulla cupiditate laudantium? Non officiis quibusdam, harum laudantium, alias eos soluta est eius ea doloremque ullam quis.
+        </p>
+        <p className="mx-auto leading-relaxed text-base mb-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quos vero placeat doloribus suscipit provident odit reprehenderit excepturi odio, distinctio commodi dolorum, quia laborum quo molestiae perspiciatis porro tempore architecto facere, quisquam eveniet nulla cupiditate laudantium? Non officiis quibusdam, harum laudantium, alias eos soluta est eius ea doloremque ullam quis.
+        </p>
+        <p className="mx-auto leading-relaxed text-base mb-4">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque voluptatibus a maiores adipisci accusamus, incidunt veritatis animi nisi ut commodi esse accusantium facilis eum porro necessitatibus. In quam earum, natus inventore assumenda illo eum tempora praesentium vitae sit possimus quas commodi sapiente veritatis obcaecati. Doloremque recusandae odit vel ea quibusdam reiciendis illo. Nostrum eos quia eveniet ex unde aliquid necessitatibus consequatur ducimus sit pariatur magnam vitae aspernatur corrupti rem esse voluptatum nemo architecto sint quasi, rerum nesciunt nobis maxime et! Voluptatum in illo ex architecto delectus minus nostrum pariatur laborum, minima dolores laboriosam voluptatem corrupti aliquam doloribus aut! Ad, quibusdam.
+        </p>
+      </div>
   )
 }
 
